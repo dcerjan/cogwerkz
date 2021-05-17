6aecf0388e7dc7bf55e1734bdc397e9fecf74015
dcerjan <domagoj.cerjan@gmail.com>
2021-05-17 22:28:01 +0200

    Draw rarity, render rarity in detailed card slot

--- data/card_concept/ItemCardConcept.gd
+++ data/card_concept/ItemCardConcept.gd
@@ -3,16 +3,11 @@ class_name ItemCardConcept
 
 export(int, FLAGS, 'Life', 'Order', 'Balance', 'Chaos', 'Death') var affinity = 0b0
 
-enum EquipmentToken {
-	None       = 0b0000000000000000,
-	Weapon     = 0b0000000000000001,
-	Armor      = 0b0000000000000010,
-	Shield     = 0b0000000000000100,
-	Helmet     = 0b0000000000001000,
-	Greaves    = 0b0000000000010000,
-	Cape       = 0b0000000000100000,
-	Consumable = 0b0000000001000000,
-	Trinket    = 0b0000000010000000,
-	Gauntlets  = 0b0000000100000000,
-}
-export(Array, EquipmentToken) var cost = []
+
+export(Array, CommonConcept.EquipmentToken) var cost = []
+
+func can_slot_into(character, slot) -> bool:
+	return (
+		.can_slot_into(character, slot) ||
+		slot.type == CommonConcept.SlotType.Equipment
+	)
--- ui/character_sheet/slots/DetailCardSlot.gd
+++ ui/character_sheet/slots/DetailCardSlot.gd
@@ -6,5 +6,14 @@ func refresh() -> void:
 	.refresh()
 	if card == null:
 		if has_node('CardName'): get_node('CardName').text = ''
+		if has_node('Rarity'): get_node('Rarity').modulate = Color(0x00000000)
 	else:
 		if has_node('CardName'): get_node('CardName').text = card.name
+		if has_node('Rarity'):
+			var color: Color = Color(0x00000000)
+			match card.rarity:
+				CommonConcept.Rarity.Common: color = Color(0x000000ff)
+				CommonConcept.Rarity.Uncommon: color = Color(0xffffffff)
+				CommonConcept.Rarity.Rare: color = Color(0xffe300ff)
+				CommonConcept.Rarity.Mythic: color = Color(0x8617ffff)
+			get_node('Rarity').modulate = color
