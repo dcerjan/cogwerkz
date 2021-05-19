401b239bebf6e85f046bbbc2c12c93a31ac9070e
dcerjan <domagoj.cerjan@gmail.com>
2021-05-19 21:09:34 +0200

    Start wokring on card preview, add smaller ninepatch

--- data/card_concept/CardConcept.gd
+++ data/card_concept/CardConcept.gd
@@ -4,16 +4,7 @@ class_name CardConcept
 
 export(String) var name = 'Unnamed Card'
 
-# because https://github.com/godotengine/godot/issues/14681
-# enums can't be referenced in export statements from other files,
-# literally copy-pasta it here
-enum Rarity {
-	Common   = 0b0001
-	Uncommon = 0b0010
-	Rare     = 0b0100
-	Mythic   = 0b1000
-}
-export(Rarity) var rarity = CommonConcept.Rarity.Common setget _set_rarity
+export(CommonConcept.Rarity) var rarity = CommonConcept.Rarity.Common setget _set_rarity
 export(Texture) var icon = null setget _set_icon
 
 func _set_rarity(new_rarity) -> void:
